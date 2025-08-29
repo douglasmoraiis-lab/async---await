c4f23c1dee3e2fb4e25582321e9e1da30387b343
Douglas de Morais <seu-email-aqui@exemplo.com>
2025-08-29 16:40:43 -0300

    feat: new files

---
+++ For/atv5.py
@@ -0,0 +1,10 @@
+lista_produtos = ["iphone", "ipad", "notebook"]
+
+for produto in lista_produtos:
+  print(produto)
+
+listas_precos =[1500, 2000, 3000]
+
+for preco in listas_precos:
+  print(preco + 50)
+  
